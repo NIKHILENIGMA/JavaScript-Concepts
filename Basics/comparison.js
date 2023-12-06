e35e8b4c4ae974e2596451924a1bb522ba0c161f
nikhilenigma <nickharmalkar18@gmail.com>
2023-12-06 15:06:38 +0530

    comparison

---
+++ Basics/comparison.js
@@ -0,0 +1,25 @@
+//@ Basic Comparison
+
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+// console.log(2 == 1);
+
+
+//@ Advance Comparison
+
+console.log("2" != 2); /// false 
+console.log("2" == 2); /// true
+
+console.log("2" >= 2); /// true
+console.log("2" <= 2); /// true
+
+console.log("2" < 2); /// false
+console.log("2" > 2); /// false
+
+//@ strick check
+
+console.log("2" === 2); /// false
+
