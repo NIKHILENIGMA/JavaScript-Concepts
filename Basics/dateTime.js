93428ce2ae43c4c0d2fd5048bbb8ee34f73d6d41
nikhilenigma <nickharmalkar18@gmail.com>
2023-12-06 20:12:52 +0530

    date time output update

--- Basics/dateTime.js
+++ Basics/dateTime.js
@@ -1,8 +1,14 @@
 
 let myDate = new Date()
 console.log(myDate.toString());
+/// Output: Wed Dec 06 2023 19:12:58 GMT+0530 (India Standard Time)
 console.log(myDate.toDateString());
+/// Output: Wed Dec 06 2023
 console.log(myDate.toISOString());
+/// Output: 2023-12-06T13:42:58.394Z
 console.log(myDate.toJSON());
+/// Output: 2023-12-06T13:42:58.394Z
 console.log(myDate.toLocaleDateString());
+/// Output: 6/12/2023
 console.log(myDate.toTimeString());
+/// Output: 19:12:58 GMT+0530 (India Standard Time)
