5e10d5959f001e054d04c74f72ce4c2be5be0630
nikhilenigma <nickharmalkar18@gmail.com>
2024-04-14 17:23:27 +0530

    clear unwanted things

--- Basics 2/object.js
+++ Basics 2/object.js
@@ -1,31 +1,5 @@
 // singleton
 
 //@ Object literals
-const mySymb = Symbol("1234")
 
-const user = {
-    name: "Nikhil",
-    age:22,
-    [mySymb] : "key1",
-    location: "Mumbai",
-    email: "pefe@fapzujef.ne",
-    lastLoginDays: ["Monday", "Saturday"]
-}
-
-// # Methods to access object are
-// console.log(user.email); /// Output pefe@fapzujef.ne
-
-// console.log(user["email"]); /// Output pefe@fapzujef.ne
-// console.log(user[mySymb]); /// Output key1
-
-user.greeting = function() {
-    console.log("Hello greetings");
-}
-
-user.greeting2 = function greeting() {
-    console.log(`Hello! ${this.name} greetings`);
-}
-
-console.log(user.greeting()); ///Output Hello greetings
-console.log(user.greeting2());///Output Hello! Nikhil greetings
 
